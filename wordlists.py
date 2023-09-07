80dd07465d4907eef5dad39c69792ba2a11f6c85
snadeem03 <shaiknadeem2003@gmail.com>
2023-09-07 13:58:09 +0530

    Added wordlists.py

---
+++ wordlists.py
@@ -0,0 +1,22 @@
+# In this file we are going to define necessary wordlists needed to perform specific actions:
+
+shutdown_wordlist = ["power off", "shut", "turn off", "deactivate", "cease", "halt", "terminate", "end"]
+
+specification_wordlist = ["specifications", "configurations", "details", "specs"]
+
+network_info_wordlist = ["network information", "wifi description", "connectivity information",
+                         "local area network information"]
+
+youtube_wordlist=["open youtube", "launch youtube", "fireup youtube", "run youtube", "visit youtube", "load youtube", "Goto youtube", "navigate to youtube", "start youtube"]
+
+chat_wordlist=["open chat", "launch chat", "fireup chat", "run chat", "visit chat", "load chat", "Goto chat", "navigate to chat", "start chat"]
+
+wikipedia_wordlist=['wiki mode',"wikipedia mode"]
+
+speedtest_wordlist=["run speed test","check internet speed","test internet speed",
+                    "how fast is my internet","measure broadband speed","internet speed check","speed test","ping test","bandwidth test","network speed test","speedometer","speed guage","internet connection speed","internet performance","evaluate network speed","assess internet speed","calculate data transfer rate"]
+
+time_wordlist=["what time is it","tell me the time","current time please","what is the time now","time please","give me the time","i need to know the time","what is the time","time check","could you tell me the current time","time right now"]
+
+
+
